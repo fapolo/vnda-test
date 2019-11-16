8db751389324b747f8d637ae5a91c3586fed604a
Fabricio Polo <fabricio.polo@gmail.com>
2019-11-16 17:13:40 -0300

    add base markup + icons

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".slider").slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear"
+  });
+});
