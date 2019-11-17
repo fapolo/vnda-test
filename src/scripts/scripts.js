91995e578e52a5b668887f4a392aff0fba493394
Fabricio Polo <fabricio.polo@gmail.com>
2019-11-17 12:53:30 -0300

    add menu styles

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,12 @@
+const nav = document.querySelector(".nav");
+const toggle = document.querySelector(".nav__toggle");
+const hamburger = document.querySelector(".nav__toggle .hamburger");
+
+toggle.addEventListener("click", function() {
+  nav.classList.toggle("open-nav");
+  hamburger.classList.toggle("open-hamburger");
+});
+
 $(document).ready(function() {
   $(".slider").slick({
     dots: true,
